16f418a202a96044a9743fa29e44bd30af153833
SunWoo Lee <suntyru@gmail.com>
2023-08-16 03:10:51 +0900

    modify some css, js

--- js/custom.js
+++ js/custom.js
@@ -56,7 +56,8 @@ $(function() {
       breakpoint: 768,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplaySpeed: 1500
       }
     }]
   })
